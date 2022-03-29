af113bf3dd5f4882b8bca38cd054e567f879cff4
Thinkedou <r.enguehard@gmail.com>
2022-03-29 10:20:54 +0200

    initialisation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+      <div class="card">
+          <div class="card-image"></div>
+          <div class="card-text">
+            <span class="job">{{teams[0].jobTitle}}</span>
+            <h2>FirstName &amp; LastName</h2>
+            <p>Petit Laïus</p>
+          </div>
+          <div class="card-stats">
+            <div class="stat">
+              <div class="techno">{{teams[0].stack[0].label}}</div>
+              <div class="level">{{displayLevel(teams[0].stack[0].level)}}</div>
+            </div>
+            <div class="stat border">
+              <div class="techno">Techno</div>
+              <div class="level">⭐⭐⭐</div>
+            </div>
+          </div>
+        </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {teams} from './assets/static/js/appTeams.js'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data:()=>({
+      teams,
+      insideMethodsData:'Choupichou'
+  }),
+  methods:{
+      displayLevel(level){
+          const starString = '⭐'
+          return starString.repeat(level)
+      },
+      preFormat(){
+          return true
+      }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/static/css/card-styles.css';
+
 </style>
